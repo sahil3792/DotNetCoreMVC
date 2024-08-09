25c9773cb287532f64adc85e3c6b7211a9ac942e
sahil3792 <sahilharshalv@gmail.com>
2024-08-09 17:15:42 +0530

    Initial Commit

---
+++ CodeFirstApproachSahil/Controllers/EmpController.cs
@@ -0,0 +1,35 @@
+﻿using CodeFirstApproachSahil.Data;
+using CodeFirstApproachSahil.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirstApproachSahil.Controllers
+{
+    public class EmpController : Controller
+    {
+        public readonly ApplicationDbContext db;
+
+        public EmpController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var obj=db.emps.ToList();
+            return View(obj);
+        }
+
+        public IActionResult AddEmp()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult AddEmp(Emp e)
+        {
+            db.emps.Add(e);
+            db.SaveChanges();
+            TempData["Msg"] = "Emp Added Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CodeFirstApproachSahil/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstApproachSahil.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproachSahil.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options ): base(options) 
+        {
+            
+        }
+
+        public DbSet< Emp > emps { get; set; }
+
+    }
+}
